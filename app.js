e49458f847d05b1b9c6da047f3973973ca5401a2
Rodrigo Machado <rodrigotmo@gmail.com>
2022-06-30 22:32:09 -0300

    changed sql queries to raw

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
 const path = require('path');
-//const db = require('./models/db');
+const db = require('./models/db');
 const Curriculo = require('./models/Curriculo');
 const bodyParser = require('body-parser');
+const QueryTypes  = require('sequelize');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,8 +14,18 @@ app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-    Curriculo.findAll().then((curriculos) => {
+    /* Curriculo.findAll().then((curriculos) => {
         res.render('curriculo', {curriculos: curriculos});
+    }); */
+    db.sequelize.query(
+        'SELECT * FROM curriculos;',
+        {
+            type: QueryTypes.SELECT,
+        }
+    ).then((curriculos) => {
+        res.render("curriculo", { curriculos: curriculos[0] });
+    }).catch((error) => {
+        console.log('Erro ao consultar registros', error);
     });
 });
 
@@ -23,7 +34,7 @@ app.get('/cadastrar-curriculo', function(req, res) {
 });
 
 app.get('/consultar/:id', function(req, res) {
-    Curriculo.findAll({
+    /* Curriculo.findAll({
         where: {
             id: req.params.id
         }
@@ -33,17 +44,47 @@ app.get('/consultar/:id', function(req, res) {
     })
     .catch((error) => {
       console.log('Erro ao consultar registro', error);
+    }); */
+    db.sequelize.query(
+        'SELECT * FROM `curriculos` WHERE `id` = ?;',
+        {
+            model: Curriculo,
+            replacements: [req.params.id],
+            type: QueryTypes.SELECT
+        }
+    ).then((curriculo) => {
+        res.render("consultar", { curriculo: curriculo[0] });
+    }).catch((error) => {
+        console.log('Erro ao consultar registro', error);
     });
 });
 
 app.post('/add-curriculo', function(req, res) {
-    Curriculo.create({
+    /* Curriculo.create({
         name: req.body.name, 
         phone: req.body.phone, 
         email: req.body.email, 
         web_address: req.body.web_address, 
         experience: req.body.experience
     }).then(() => {
+        res.redirect('/');
+        console.log('Cadastrado com sucesso');
+    }).catch((error) => {
+        res.sendStatus(400); 
+        console.log('Falha no cadastro', error);
+    }); */
+    db.sequelize.query(
+        'INSERT INTO curriculos (`name`, `phone`, `email`, `web_address`, `experience`) VALUES (?,?,?,?,?)',
+        {
+            model: Curriculo,
+            replacements: [req.body.name, 
+                req.body.phone, 
+                req.body.email, 
+                req.body.web_address, 
+                req.body.experience],
+            type: QueryTypes.INSERT
+        }
+    ).then(() => {
         res.redirect('/');
         console.log('Cadastrado com sucesso');
     }).catch((error) => {
