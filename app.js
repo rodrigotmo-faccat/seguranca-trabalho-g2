4e3cc8f1560385d8024948817c096c1ffd190e14
Lucas Sanches <sanchesluka@hotmail.com>
2022-07-02 19:02:11 -0300

    xss

--- app.js
+++ app.js
@@ -5,49 +5,51 @@ const path = require('path');
 //const db = require('./models/db');
 const Curriculo = require('./models/Curriculo');
 const bodyParser = require('body-parser');
+const xss = require('XSS')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     Curriculo.findAll().then((curriculos) => {
-        res.render('curriculo', {curriculos: curriculos});
+        res.render('curriculo', { curriculos: curriculos });
     });
 });
 
-app.get('/cadastrar-curriculo', function(req, res) {
+app.get('/cadastrar-curriculo', function (req, res) {
     res.render('cadastrar-curriculo');
 });
 
-app.get('/consultar/:id', function(req, res) {
+app.get('/consultar/:id', function (req, res) {
     Curriculo.findAll({
         where: {
             id: req.params.id
         }
     })
-    .then((curriculo) => {
-      res.render("consultar", { curriculo: curriculo[0] });
-    })
-    .catch((error) => {
-      console.log('Erro ao consultar registro', error);
-    });
+        .then((curriculo) => {
+            res.render("consultar", { curriculo: curriculo[0] });
+        })
+        .catch((error) => {
+            console.log('Erro ao consultar registro', error);
+        });
 });
 
-app.post('/add-curriculo', function(req, res) {
+app.post('/add-curriculo', function (req, res) {
     Curriculo.create({
-        name: req.body.name, 
-        phone: req.body.phone, 
-        email: req.body.email, 
-        web_address: req.body.web_address, 
-        experience: req.body.experience
+        name: xss(req.body.name),
+        phone: xss(req.body.phone),
+        email: xss(req.body.email),
+        web_address: xss(req.body.web_address),
+        experience: xss(req.body.experience),
+
     }).then(() => {
         res.redirect('/');
         console.log('Cadastrado com sucesso');
     }).catch((error) => {
-        res.sendStatus(400); 
+        res.sendStatus(400);
         console.log('Falha no cadastro', error);
     });
 });
